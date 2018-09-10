22a02c02b94127ddb813736b2b4481afad896e11
cramis1 <40275970+cramis1@users.noreply.github.com>
2018-09-10 12:27:52 -0400

    Add files via upload

--- common/utils.js
+++ common/utils.js
@@ -1,6 +1,6 @@
 
 export function getDisplayMonth(month) {
-  const strMonth =["Jan", "Feb", "Mar", "Apr.", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
+  const strMonth =["Jan", "Feb", "Mar", "Apr.", "May", "June", "July", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
   return strMonth[month];
 }
 
